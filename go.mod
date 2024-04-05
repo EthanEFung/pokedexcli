95ad7f99227aba5d673577340fbd8339a51dfc7a
Ethan Fung <ethanfung@ethans-mac-mini.lan>
2024-04-05 09:27:29 -0700

    feat: a few ui updates

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/charmbracelet/bubbles v0.18.0
 	github.com/charmbracelet/bubbletea v0.25.0
 	github.com/charmbracelet/lipgloss v0.9.1
+	github.com/muesli/reflow v0.3.0
 	github.com/qeesung/image2ascii v1.0.1
 )
 
@@ -20,7 +21,6 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
-	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/rivo/uniseg v0.4.6 // indirect
--- model_detail.go
+++ model_detail.go
@@ -7,7 +7,9 @@ import (
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/ethanefung/pokedexcli/internal/pokeapi"
+	"github.com/muesli/reflow/wordwrap"
 	"github.com/qeesung/image2ascii/convert"
 )
 
@@ -32,6 +34,56 @@ type stats struct {
 	specialAttack  string
 	specialDefense string
 	speed          string
+}
+
+type pType string
+
+const (
+	normalType   pType = "normal"
+	fireType           = "fire"
+	waterType          = "water"
+	electricType       = "electric"
+	grassType          = "grass"
+	iceType            = "ice"
+	fightingType       = "fighting"
+	poisonType         = "poison"
+	groundType         = "ground"
+	flyingType         = "flying"
+	psychicType        = "psychic"
+	bugType            = "bug"
+	rockType           = "rock"
+	ghostType          = "ghost"
+	dragonType         = "dragon"
+	darkType           = "dark"
+	steelType          = "steel"
+	fairyType          = "fairy"
+)
+
+type pTypeStyle struct {
+	pType   pType
+	bgColor string
+	color   string
+}
+
+var pTypeStylesMap = map[string]pTypeStyle{
+	"normal":   {normalType, "#A8A77A", "#FFF"},
+	"fire":     {fireType, "#EE8130", "#FFF"},
+	"water":    {waterType, "#6390F0", "#FFF"},
+	"electric": {electricType, "#F7D02C", "#FFF"},
+	"grass":    {grassType, "#7AC74C", "#FFF"},
+	"ice":      {iceType, "#96D9D6", "#FFF"},
+	"fighting": {fightingType, "#C22E28", "#FFF"},
+	"poison":   {poisonType, "#A33EA1", "#FFF"},
+	"ground":   {groundType, "#E2BF65", "#FFF"},
+	"flying":   {flyingType, "#A98FF3", "#FFF"},
+	"psychic":  {psychicType, "#F95587", "#FFF"},
+	"bug":      {bugType, "#A6B91A", "#FFF"},
+	"rock":     {rockType, "#B6A136", "#FFF"},
+	"ghost":    {ghostType, "#735797", "#FFF"},
+	"dragon":   {dragonType, "#6F35FC", "#FFF"},
+	"dark":     {darkType, "#705746", "#FFF"},
+	"steel":    {steelType, "#B7B7CE", "#FFF"},
+	"fairy":    {fairyType, "#D685AD", "#FFF"},
 }
 
 func (d detail) Init() tea.Cmd { return nil }
@@ -68,23 +120,39 @@ func (d detail) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 func (d detail) View() string {
 	var s string
-	s += fmt.Sprintf("%s #%04d\n", d.name, d.no)
+	s += fmt.Sprintf("No #%04d %s\n", d.no, toStartCase(d.name))
 	if d.asciiSprite != "" {
 		s += d.asciiSprite + "\n"
 	}
-	s += d.description + "\n"
-	s += "height: " + d.height + "\n"
-	s += "weight: " + d.weight + "\n"
+	s += strings.Repeat("-", 64) + "\n\n"
 	s += "types: "
-	for i, t := range d.types {
-		s += t
-		if i < len(d.types)-1 {
-			s += ", "
-		}
+	for _, t := range d.types {
+		pts := pTypeStylesMap[t]
+		s += pts.String()
 	}
 	s += "\n"
+	s += "height: " + d.height + "\n"
+	s += "weight: " + d.weight + "\n\n"
+	s += strings.Repeat("-", 64) + "\n\n"
+	desc := wordwrap.String(d.description, 62)
+	s += desc + "\n"
 
 	return s
+}
+
+func (pts pTypeStyle) String() string {
+	return lipgloss.NewStyle().
+		Background(lipgloss.Color(pts.bgColor)).
+		Foreground(lipgloss.Color(pts.color)).
+		Padding(0, 1).
+		Render(string(pts.pType))
+}
+
+func toStartCase(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	return strings.ToTitle((s[:1])) + strings.ToLower(s[1:])
 }
 
 func decimetresToImperialUnits(decimetres int) string {
